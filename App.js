010b65465738141c23d3db0f607dbd3ba1cc9619
tiffanyleee <76767856+tiffanyleee@users.noreply.github.com>
2022-02-10 23:25:15 -0800

    changing it back to a3

--- App.js
+++ App.js
@@ -7,26 +7,96 @@ import Colors from "./Themes/colors";
 import { Pressable, View, Image } from 'react-native';
 import Song from './Song';
 import  millisToMinutesAndSeconds  from "./utils/millisToMinuteSeconds";
-import * as React from 'react';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import MainScreen from "./screens/MainScreen";
-import DetailScreen from "./screens/DetailScreen";
-import PreviewScreen from "./screens/PreviewScreen";
-import { createStackNavigator } from '@react-navigation/stack';
-import 'react-native-gesture-handler';
 
-const Stack = createStackNavigator();
 
-export default function App({navigation}) {
-  return(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="MainScreen" component={MainScreen} options={{headerShown: false}}/>
-        <Stack.Screen name="DetailScreen" component={DetailScreen} options={{ title: 'Song Details' }}/>
-        <Stack.Screen name="PreviewScreen" component={PreviewScreen} options={{ title: 'Song Preview' }}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+// Endpoints for authorizing with Spotify
+const discovery = {
+  authorizationEndpoint: "https://accounts.spotify.com/authorize",
+  tokenEndpoint: "https://accounts.spotify.com/api/token"
+};
+
+export default function App() {
+  const [token, setToken] = useState("");
+  const [tracks, setTracks] = useState([]);
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: CLIENT_ID,
+      scopes: SCOPES,
+      // In order to follow the "Authorization Code Flow" to fetch token after authorizationEndpoint
+      // this must be set to false
+      usePKCE: false,
+      redirectUri: REDIRECT_URI
+    },
+    discovery
+  );
+
+
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { access_token } = response.params;
+      setToken(access_token);
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (token) {
+      // Authenticated, make API request
+      // TODO: Select which option you want: Top Tracks or Album Tracks
+      myTopTracks(setTracks, token);
+      //albumTracks(ALBUM_ID, setTracks, token);
+    }
+  }, [token]);
+
+
+  let contentDisplayed = null;
+
+    // given a track, render it as a Song component
+    const renderItem = ({item, index}) => {  
+      console.log(index); 
+      return (
+      <Song 
+        songIndex = {index}
+        albumImage = {item.album.images[0].url}
+        title = {item.name}
+        artist = {item.artists[0].name}
+        album = {item.album.name}
+        duration =  {millisToMinutesAndSeconds(item.duration_ms) }
+      />);
+  };
+
+  // if-else based on if authenticated or not
+  if (token) {
+    contentDisplayed = 
+    <View style={styles.container}>
+      <View style={styles.titleRow}>
+        <Image style={styles.titleLogo} source={require("./assets/spotify-logo.png")} />
+        <Text style={styles.titleText}>My Top Tracks</Text>
+      </View>
+      <FlatList
+        data = {tracks} // the array of data that the FlatList displays
+        renderItem={renderItem} // function that renders each item
+        keyExtractor={(item, index) => index.toString()} // unique key for each item
+      />
+    </View>
+
+  } else {
+    contentDisplayed = 
+    <View style={styles.button}> 
+        <Pressable onPress={promptAsync} style={styles.press}>
+          <Image style={styles.logo} source={require("./assets/spotify-logo.png")} />
+          <Text style={styles.connect}>CONNECT WITH SPOTIFY</Text>
+        </Pressable>  
+    </View>
+  };
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {contentDisplayed}
+    </SafeAreaView>
   );
 }
 
@@ -78,6 +148,7 @@ const styles = StyleSheet.create({
 
   titleRow: {
     flexDirection: 'row',
+    backgroundColor: Colors.background,
     justifyContent: 'center',
     alignContent: 'center',
   },
