b718e963778f5852c7956a85c8b0c05dadb7e776
tiffanyleee <76767856+tiffanyleee@users.noreply.github.com>
2022-02-10 23:27:00 -0800

    restoring back to a3

--- Song.js
+++ Song.js
@@ -5,56 +5,35 @@ import { myTopTracks, albumTracks } from "./utils/apiOptions";
 import { REDIRECT_URI, SCOPES, CLIENT_ID, ALBUM_ID } from "./utils/constants";
 import Colors from "./Themes/colors";
 import { Pressable, View, Image } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import * as React from 'react';
-// import HomeStack from './Screens/HomeStack';
-import { useNavigation } from '@react-navigation/native';
-import 'react-native-gesture-handler';
-import { WebView } from 'react-native-webview';
 
 
-export default function Song({songIndex, albumImage, title, artist, album, duration, externalurl, previewurl}){
-  const navigation = useNavigation(); 
+export default function Song({songIndex, albumImage, title, artist, album, duration}){
     return(
       <View style={styles.container}>
-          <Pressable onPress={(e) => {e.stopPropagation(); navigation.navigate("PreviewScreen", {previewurl: previewurl})}} style={styles.icon}> 
-            <Ionicons name="play-circle" size={32} color="green" />
-          </Pressable>
-
-          <Pressable onPress={(e) => {e.stopPropagation(); navigation.navigate("DetailScreen", {externalurl: externalurl})}} style={styles.details}> 
-            <Image style={styles.albImg} source={{uri: albumImage}} />  
-            <View style={styles.songInfo}>
-              <Text style={styles.song} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
-              <Text style={styles.artist} numberOfLines={1} ellipsizeMode="tail" >{artist}</Text>
-            </View>
-            <Text style={styles.album} numberOfLines={2} ellipsizeMode="tail">{album}</Text>
-            <Text style={styles.duration}>{duration}</Text>
-          </Pressable>
+          <Text style={styles.numbering}>{songIndex + 1}</Text>
+          <Image style={styles.albImg} source={{uri: albumImage}} />  
+          <View style={styles.songInfo}>
+            <Text style={styles.song} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
+            <Text style={styles.artist} numberOfLines={1} ellipsizeMode="tail" >{artist}</Text>
+          </View>
+          <Text style={styles.album} numberOfLines={2} ellipsizeMode="tail">{album}</Text>
+          <Text style={styles.duration}>{duration}</Text>
       </View>
       
     );
   }
 
-  
-
-
 const styles = StyleSheet.create({
     container: {    
-        padding: 8,    
+        padding: 6,    
         flex: 1,
         flexDirection: 'row',
       },
     
-      icon: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '8%',
-      },
-
-      details: {
-        width: '100%',
-        flex: 1,
-        flexDirection: 'row',
+      numbering: {
+        textAlignVertical: 'center',
+        width: '5%',
+        color: 'grey',
       },
 
       albImg: {
@@ -64,7 +43,7 @@ const styles = StyleSheet.create({
     
       songInfo: {
         padding: 5,
-        width: '40%',
+        width: '35%',
       },
     
       song:{
